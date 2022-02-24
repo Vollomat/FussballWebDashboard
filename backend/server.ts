4b4377a3135b53173defe057fbd5e81295cfee03
Vollomat <73344372+Vollomat@users.noreply.github.com>
2022-02-24 18:48:09 +0100

    Add files via upload

---
+++ backend/server.ts
@@ -0,0 +1,24 @@
+import { opine } from "https://deno.land/x/opine@2.1.1/mod.ts";
+import { Request } from 'https://deno.land/x/request@1.3.2/mod.ts'
+
+const app = opine();
+
+const pathToIndexHTMLFile = `${Deno.cwd()}/frontend/index.html`
+
+app.get("/", function (req, res) {
+    res.sendFile(pathToIndexHTMLFile); // consider providing the index.html via github pages as an alternative
+});
+
+app.get("/getISSPosition", async function (req, res) {
+    const result: any = await Request.get('https://www.openligadb.de/api/getavailableteams/bl1/2021')
+
+    res.send(`the ISS is above: ${JSON.stringify(result)}`);
+    console.log(`${JSON.stringify(result)}`);
+});
+
+const port = 3000
+
+app.listen(
+    port,
+    () => console.log(`server has started on http://localhost:${port} 🚀`),
+);
