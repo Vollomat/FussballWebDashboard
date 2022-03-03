ce35e980515e5ac359aa99017e6099b0a1c99b58
Vollomat <73344372+Vollomat@users.noreply.github.com>
2022-03-03 15:18:06 +0100

    Create App.svelte

---
+++ docs/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import UserInput from "@/components/UserInput.svelte";
+  import { fade } from "svelte/transition";
+  import Unterseite from "@/components/Unterseite.svelte"
+  let name = "";
+  
+</script>
+
+<main transition:fade>
+
+  <p><br /></p>
+
+  <UserInput />
+  
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
