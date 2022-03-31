22b6efc1619db3f4ea45594ce3a4fc4b59e4ce32
Michael <michael.spengler@sap.com>
2022-03-31 17:53:10 +0200

    tidy up part 1

--- frontend/example-ui-in-svelte-using-snel/src/components/MiddlePart/Spielplan.svelte
+++ frontend/example-ui-in-svelte-using-snel/src/components/MiddlePart/Spielplan.svelte
@@ -1,116 +1,141 @@
 <script>
     import { Request } from "https://deno.land/x/request@1.3.2/mod.ts";
+    import { onMount } from "svelte";
 
-    let SpieleDerSaison = "";
+    let spieleDerSaison;
     let promise;
     let promiseSpieltage;
-    let AuswahloptionenSpieltage;
-    let AusgewaehlterSpieltag = "";
+    let ausgewaehlterSpieltag = 0;
     let hasmatchresult = false;
+    let filteredGames = [];
 
-    promise = getSpielplan();
+    // promise = getSpielplan();
+
+    onMount(async () => {
+        await getSpielplan();
+        filteredGames = spieleDerSaison;
+        alert(spieleDerSaison.length);
+    });
 
     async function getSpielplan() {
         let uri = "https://www.openligadb.de/api/getmatchdata/bl1/2021";
         const result = await Request.get(uri);
-        SpieleDerSaison = result;
-        console.log(SpieleDerSaison)
+        spieleDerSaison = result;
+        console.log(spieleDerSaison);
     }
-    
 
-    function getSpieleDesSpieltags(){
-        
-        AuswahloptionenSpieltage = document.getElementById("SP").value;
-        AuswahloptionenSpieltage = parseInt(AuswahloptionenSpieltage);
-        alert(AuswahloptionenSpieltage);
-        console.log(SpieleDerSaison.GroupOrderID)
-        for(let i = 0; i < SpieleDerSaison.length; i++) {
-            if (AuswahloptionenSpieltage === SpieleDerSaison[i].GroupOrderID){
-                AusgewaehlterSpieltag.push(SpieleDerSaison[i]);
-                console.log("bin in methode")
-                console.log(SpieleDerSaison[i].GroupOrderID)
+    function getSpieleDesSpieltags() {
+        filteredGames = [];
+        console.log(spieleDerSaison.GroupOrderID);
+
+        for (const spielDerSaison of spieleDerSaison) {
+            console.log(JSON.stringify(spielDerSaison));
+            if (ausgewaehlterSpieltag === spielDerSaison.Group.GroupOrderID) {
+                filteredGames.push(spielDerSaison);
+                console.log("bin in methode");
             }
-            
         }
-        
-        SpieleDerSaison = AusgewaehlterSpieltag;
 
+        // for (let i = 0; i < spieleDerSaison.length; i++) {
+        //     console.log(JSON.stringify(spieleDerSaison[i]));
+        //     if (
+        //         ausgewaehlterSpieltag === spieleDerSaison[i].Group.GroupOrderID
+        //     ) {
+        //         filteredGames.push(spieleDerSaison[i]);
+        //         console.log("bin in methode");
+        //     }
+        // }
+
+        filteredGames;
     }
 
-    function getSpiele(){
-        promiseSpieltage = getSpieleDesSpieltags();
-    }
+    // function getSpiele() {
+    //     promiseSpieltage = getSpieleDesSpieltags();
+    // }
 
-
-    function checkcount(spielplan){
-        console.log("bin in checkcount")
-        if(spielplan.MatchResults.length > 0){
-            return true
+    function checkcount(spielplan) {
+        console.log("bin in checkcount");
+        if (spielplan.MatchResults.length > 0) {
+            return true;
         }
-            return false
+        return false;
     }
-
 </script>
 
 <center>
-{#await promiseSpieltage};
-{:then}
-    <div allign="center">
-        <h2>&nbsp Welchen Spieltag wollen Sie sehen? &nbsp</h2>
-        <input list="Spieltage" id="SP" name="Spieltage" />  
-        
-        <datalist id="Spieltage">
+    {#await promiseSpieltage};
+    {:then}
+        <div allign="center">
+            <h2>&nbsp Welchen Spieltag wollen Sie sehen? &nbsp</h2>
+            <input
+                bind:value={ausgewaehlterSpieltag}
+                list="Spieltage"
+                type="number"
+                name="Spieltage"
+            />
 
-        {#each Array(34) as _, index (index)}
-            <option value= {index + 1}  Spieltag >
-        {/each}
-        </datalist>
+            <datalist id="Spieltage">
+                {#each Array(34) as _, index (index)}
+                    <option value={index + 1} Spieltag />{/each}
+            </datalist>
 
-        <button id="getSpielplan" on:click|once={getSpiele} type="getSpielplan">Spieltag anzeigen</button>
-        
-    </div>
-{/await}
+            <button
+                id="getSpielplan"
+                on:click={getSpieleDesSpieltags}
+                type="getSpielplan">Spieltag anzeigen</button
+            >
+        </div>
+    {/await}
 
-{#await promiseSpieltage};
-{:then}
-    <div allign="center">
-        <table border="1">
-            <tr>
-            <td><h2>&nbsp Spieltag &nbsp</h2></td>
-            <td style="background-color:#243D85"><h2>&nbsp Datum &nbsp</h2></td>
-            <td><h2>&nbsp Team 1 &nbsp</h2></td>
-            <td style="background-color:#243D85"><h2>&nbsp Team 2 &nbsp</h2></td>
-            <td><h2>&nbsp Ergebnis &nbsp</h2></td>
-            </tr>
-            {#each SpieleDerSaison as spielplan}
-                
+    {#await promiseSpieltage};
+    {:then}
+        <div allign="center">
+            <table border="1">
                 <tr>
-                <td>&nbsp {spielplan.Group.GroupOrderID} &nbsp</td>
-                <td style="background-color:#243D85">&nbsp {spielplan.MatchDateTime} &nbsp</td> 
-                <td>&nbsp {spielplan.Team1.TeamName} &nbsp</td>
-                <td style="background-color:#243D85">&nbsp {spielplan.Team2.TeamName} &nbsp</td>
-                {#if checkcount(spielplan)}
-                    <td>&nbsp {spielplan.MatchResults[0].PointsTeam1} : {spielplan.MatchResults[0].PointsTeam2} &nbsp</td>
-                {:else }
-                <td>&nbsp --- : --- &nbsp</td>
-                {/if}
+                    <td><h2>&nbsp Spieltag &nbsp</h2></td>
+                    <td style="background-color:#243D85"
+                        ><h2>&nbsp Datum &nbsp</h2></td
+                    >
+                    <td><h2>&nbsp Team 1 &nbsp</h2></td>
+                    <td style="background-color:#243D85"
+                        ><h2>&nbsp Team 2 &nbsp</h2></td
+                    >
+                    <td><h2>&nbsp Ergebnis &nbsp</h2></td>
                 </tr>
-            {/each}
-        </table>
-    
-    </div>
-{/await}
+                {#each filteredGames as filteredGame}
+                    <tr>
+                        <td>&nbsp {filteredGame.Group.GroupOrderID} &nbsp</td>
+                        <td style="background-color:#243D85"
+                            >&nbsp {filteredGame.MatchDateTime} &nbsp</td
+                        >
+                        <td>&nbsp {filteredGame.Team1.TeamName} &nbsp</td>
+                        <td style="background-color:#243D85"
+                            >&nbsp {filteredGame.Team2.TeamName} &nbsp</td
+                        >
+                        {#if checkcount(filteredGame)}
+                            <td
+                                >&nbsp {filteredGame.MatchResults[0]
+                                    .PointsTeam1} :
+                                {filteredGame.MatchResults[0].PointsTeam2} &nbsp</td
+                            >
+                        {:else}
+                            <td>&nbsp --- : --- &nbsp</td>
+                        {/if}
+                    </tr>
+                {/each}
+            </table>
+        </div>
+    {/await}
 </center>
 
 <style>
-td {
-    text-align: center;
-    color: white;
-    font-family: "Lato", sans-serif;
-}
+    td {
+        text-align: center;
+        color: white;
+        font-family: "Lato", sans-serif;
+    }
 
-table{
-    width: 60%;
-}
-    
+    table {
+        width: 60%;
+    }
 </style>
