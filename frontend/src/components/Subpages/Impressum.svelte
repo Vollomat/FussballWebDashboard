91a0179adbf04e6d6797096bec6cf1acf84958b2
Vollomat <patrick.vollstedt@gmx.de>
2022-04-07 16:43:10 +0200

    impressum

--- frontend/src/components/Subpages/Impressum.svelte
+++ frontend/src/components/Subpages/Impressum.svelte
@@ -1,1 +1,37 @@
+<script>
 
+</script>
+
+<center>
+<h2>Impressum</h2>
+
+<div class="Impressum">
+<p>
+FußballDashboard GmbH <br/>
+Scharnhorststraße 4 <br/>
+40477 Düsseldorf <br/>
+Deutschland <br/>
+</p>
+
+<h3>Kontakt</h3>
+<p>
+E-Mail: patrick.vollstedt@fussballdashboard.de <br/>
+Telefon: +49 176 656 305 14 <br/>
+<br/>
+<br/>
+Geschäftsführung: Patrick Vollstedt
+</p>
+
+
+</div>
+
+</center>
+
+<style>
+
+center{
+font-family: "Lato", sans-serif;
+color: white;
+}
+
+</style>
